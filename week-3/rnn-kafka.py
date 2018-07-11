30049a8a93a1aca3f79d65ad863f7686a21d4302
yash chauhan <yashchauhan281@gmail.com>
2018-07-12 00:45:37 +0530

    manual rnn test completed for kafka dataset

--- week-3/rnn-kafka.py
+++ week-3/rnn-kafka.py
@@ -6,8 +6,8 @@ chars = list(set(data))
 data_size, vocab_size = len(data),len(chars)
 
 char_to_ix = {ch:i for i,ch in enumerate(chars)}
-ix_to_char = {ch:i for i,ch in enumerate(chars)}
-
+ix_to_char = {i:ch for i,ch in enumerate(chars)}
+print(ix_to_char)
 vector_for_char_a = np.zeros((vocab_size, 1))
 vector_for_char_a[char_to_ix['a']] = 1
 
@@ -29,7 +29,7 @@ def lossFunc(inputs, targets, hprev):
     for t in range(len(inputs)):
         xs[t] = np.zeros((vocab_size, 1))
         xs[t][inputs[t]] = 1
-        hs[t] = np.tanh(np.dot(Wxh,xs[t])) + np.dot(Whh, hs[t-1]) + bh
+        hs[t] = np.tanh(np.dot(Wxh,xs[t]) + np.dot(Whh, hs[t-1]) + bh)
         ys[t] = np.dot(Why,hs[t]) + by
         ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t]))
         loss += -np.log(ps[t][targets[t],0])
@@ -61,7 +61,7 @@ def sample(h,seed_ix,n):
     x[seed_ix] = 1
     ixes = []
     for t in range(n):
-        h = np.tanh(np.dot(Wxh,x)) + np.dot(Whh,h)
+        h = np.tanh(np.dot(Wxh,x) + np.dot(Whh,h) + bh)
         y = np.dot(Why,h) + by
         p = np.exp(y) / np.sum(np.exp(y))
         ix = np.random.choice(range(vocab_size),p = p.ravel())
@@ -70,7 +70,44 @@ def sample(h,seed_ix,n):
         ixes.append(ix)
     txt = ''.join(ix_to_char[ix] for ix in ixes)
     print ('----\n %s \n -----' % (txt, ))
-hprev = np.zeros((hidden_size, 1))
-sample(hprev,char_to_ix['a'],200)
 
-       
+hprev = np.zeros((hidden_size, 1))
+# #sample(hprev,char_to_ix['a'],200)
+
+# p = 0 
+# inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
+# print("inputs" , inputs)
+# targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
+# print("outputs" , targets)
+
+n, p = 0, 0
+mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
+mbh, mby = np.zeros_like(bh), np.zeros_like(by) # memory variables for Adagrad                                                                                                                
+smooth_loss = -np.log(1.0/vocab_size)*seq_length # loss at iteration 0                                                                                                                        
+while n<=1000*100:
+  # prepare inputs (we're sweeping from left to right in steps seq_length long)
+  # check "How to feed the loss function to see how this part works
+  if p+seq_length+1 >= len(data) or n == 0:
+    hprev = np.zeros((hidden_size,1)) # reset RNN memory                                                                                                                                      
+    p = 0 # go from start of data                                                                                                                                                             
+  inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
+  targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
+
+  # forward seq_length characters through the net and fetch gradient                                                                                                                          
+  loss, dWxh, dWhh, dWhy, dbh, dby, hprev = lossFunc(inputs, targets, hprev)
+  smooth_loss = smooth_loss * 0.999 + loss * 0.001
+
+  # sample from the model now and then                                                                                                                                                        
+  if n % 1000 == 0:
+    print ('iter %d, loss: %f' % (n, smooth_loss)) # print progress)
+    sample(hprev,inputs[0],200)
+
+  # perform parameter update with Adagrad                                                                                                                                                     
+  for param, dparam, mem in zip([Wxh, Whh, Why, bh, by],
+                                [dWxh, dWhh, dWhy, dbh, dby],
+                                [mWxh, mWhh, mWhy, mbh, mby]):
+    mem += dparam * dparam
+    param += -learning_rate * dparam / np.sqrt(mem + 1e-8) # adagrad update                                                                                                                   
+
+  p += seq_length # move data pointer                                                                                                                                                         
+  n += 1 # iteration counter
